fe5b6ec203e06b8ab5668001bde2f39955b22749
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 21:31:40 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -2,7 +2,7 @@ class reateStudents < ActiveRecord::Migration[5.1]
   def change
    add_column :artists do |t|
      t.integer :grade
-     t.text :birthdate
+     t.string :birthdate
    end
  end
 end
